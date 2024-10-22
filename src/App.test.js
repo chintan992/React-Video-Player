451a303b66ae6939568b226e6b90557b4f050d77
Chintan Rathod <chintanr21@gmail.com>
2024-10-22 02:04:07 -0400

    Delete src/App.test.js

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Welcome to LetsStream heading', () => {
-  render(<App />);
-  const headingElement = screen.getByText(/welcome to letsstream/i);
-  expect(headingElement).toBeInTheDocument();
-});
